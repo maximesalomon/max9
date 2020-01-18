eaf9f747ebba387250fa5da246dc81f0b7d68fe0
Maxime Salomon <maxime@croissant.io>
2020-01-18 01:59:25 +0100

    axios post wip

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import axios from 'axios';
+
+const getToken = () => {
+  axios.post('https://api.instagram.com/oauth/authorize', {
+    client_id: '814423029033574',
+    redirect_uri: 'https://croissant.io',
+    scope: 'user_profile,user_media',
+    response_type: 'code'
+  })
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+}
 
 const App = () => {
   return (
     <div className="App">
       <h1>Max9 <span role="img" aria-label="fire emoji">🔥</span></h1>
+      <button onClick={() => getToken()}>Login</button>
     </div>
   );
 }
