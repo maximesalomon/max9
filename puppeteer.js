44cdf68b0d43de3c3349cae2fbb2a4863fc6a85e
Maxime Salomon <maxime@croissant.io>
2020-01-21 16:11:54 +0100

    use page.$eval

--- puppeteer.js
+++ puppeteer.js
@@ -7,8 +7,7 @@ const puppeteer = require("puppeteer");
   const page = await browser.newPage();
   await page.setViewport({ width: 1440, height: 900 });
   await page.goto("https://www.instagram.com/p/B6yWe5rCpkP/", { waitUntil: "networkidle2" });
-  const likes_count = await page.evaluate(() => document.querySelector('.Nm9Fw button span').innerText);
-//   await page.click('div')
+  const likes_count = await page.$eval('.Nm9Fw button span', el => el.innerText)
   console.log(likes_count)
   await browser.close();
 })();
