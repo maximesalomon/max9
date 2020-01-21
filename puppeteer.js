bd25ccc0c0f0a83b6227f5453cac274ad1645ab9
Maxime Salomon <maxime@croissant.io>
2020-01-21 13:13:37 +0100

    puppeteer likes_count

--- puppeteer.js
+++ puppeteer.js
@@ -1,15 +1,14 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch(
-      {
-          headless: false,
-      }
-    );
+  const browser = await puppeteer.launch({
+    headless: true
+  });
   const page = await browser.newPage();
-  await page.setViewport({ width: 1440, height: 900 })
-  await page.goto('https://instagram.com');
-  await page.screenshot({path: 'instagram.png'});
-
+  await page.setViewport({ width: 1440, height: 900 });
+  await page.goto("https://www.instagram.com/p/B6yWe5rCpkP/", { waitUntil: "networkidle2" });
+  const likes_count = await page.evaluate(() => document.querySelector('.Nm9Fw button span').innerText);
+//   await page.click('div')
+  console.log(likes_count)
   await browser.close();
 })();
