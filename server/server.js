2669d206c41755511ac3b71bc5502be354f2d126
Maxime Salomon <maxime@croissant.io>
2020-01-22 00:55:13 +0100

    add body-parser to use.req.body.permalink

--- server/server.js
+++ server/server.js
@@ -1,25 +1,39 @@
-const express = require('express');
+const express = require("express");
 const puppeteer = require("puppeteer");
+const bodyParser = require("body-parser");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send('Hello from Express');
+server.use(bodyParser.json());
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
 });
 
-server.get('/max9', async (req, res) => {
-    const browser = await puppeteer.launch({
-        headless: true
-    });
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1440, height: 900 });
-    await page.goto('https://www.instagram.com/p/B6yWe5rCpkP', { waitUntil: "networkidle2" });
-    const likes_count = await page.$eval('.Nm9Fw button span', el => el.innerText)
-    await browser.close();
-    // console.log(likes_count)
-    res.status(200).json(likes_count)
+server.get("/", (req, res) => {
+  res.send("Hello from Express");
+});
+
+server.get("/max9", async (req, res) => {
+  console.log(req.body.permalink);
+  const browser = await puppeteer.launch({
+    headless: true
   });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1440, height: 900 });
+  await page.goto(req.body.permalink, {
+    waitUntil: "networkidle2"
+  });
+  const likes_count = await page.$eval(
+    ".Nm9Fw button span",
+    el => el.innerText
+  );
+  await browser.close();
+  console.log(likes_count)
+  res.status(200).json(likes_count);
+});
 
 server.listen(7000, () =>
-  console.log('Server running on http://localhost:7000')
+  console.log("Server running on http://localhost:7000")
 );
