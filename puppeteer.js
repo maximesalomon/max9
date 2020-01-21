4ec0ba6199d8956312ccf317ff4bedb757d95abf
Maxime Salomon <maxime@croissant.io>
2020-01-21 16:27:15 +0100

    add getLikesCount async fn w/ permalink as parameter

--- puppeteer.js
+++ puppeteer.js
@@ -1,13 +1,16 @@
 const puppeteer = require("puppeteer");
 
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: true
-  });
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1440, height: 900 });
-  await page.goto("https://www.instagram.com/p/B6yWe5rCpkP/", { waitUntil: "networkidle2" });
-  const likes_count = await page.$eval('.Nm9Fw button span', el => el.innerText)
-  console.log(likes_count)
-  await browser.close();
-})();
+const getLikesCount = async (permalink) => {
+    const browser = await puppeteer.launch({
+        headless: true
+    });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1440, height: 900 });
+    await page.goto(permalink, { waitUntil: "networkidle2" });
+    const likes_count = await page.$eval('.Nm9Fw button span', el => el.innerText)
+    await browser.close();
+    console.log(likes_count)
+    return likes_count
+}
+
+getLikesCount('https://www.instagram.com/p/B6yWe5rCpkP/')
