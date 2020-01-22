bf91432589e5398ea6354a5c2595f3de95e57e50
Maxime Salomon <maxime@croissant.io>
2020-01-22 01:41:27 +0100

    get user pictures likes_count wip

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const server = express();
 
@@ -15,23 +16,55 @@ server.get("/", (req, res) => {
   res.send("Hello from Express");
 });
 
-server.get("/max9", async (req, res) => {
-  console.log(req.body.permalink);
-  const browser = await puppeteer.launch({
-    headless: true
-  });
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1440, height: 900 });
-  await page.goto(req.body.permalink, {
-    waitUntil: "networkidle2"
-  });
-  const likes_count = await page.$eval(
-    ".Nm9Fw button span",
-    el => el.innerText
-  );
-  await browser.close();
-  console.log(likes_count)
-  res.status(200).json(likes_count);
+server.get("/api/max9/", async (req, res) => {
+  console.log(req.body.user_id);
+  console.log(req.body.access_token);
+  await axios
+    .get("https://graph.instagram.com/me/media", {
+      params: {
+        fields:
+          "id,timestamp,media_type,media_url,thumbnail_url,permalink,caption",
+        access_token: req.body.access_token
+      }
+    })
+    .then(res => {
+      console.log(res.data.data);
+      const pics = res.data.data.map(async pic => {
+            const browser = await puppeteer.launch({
+            headless: true
+          });
+          const page = await browser.newPage();
+          await page.setViewport({ width: 1440, height: 900 });
+          await page.goto(pic.permalink, {
+            waitUntil: "networkidle2"
+          });
+          const likes_count = await page.$eval(
+            ".Nm9Fw button span",
+            el => el.innerText
+          );
+          console.log(`${pic.permalink} received ${likes_count} likes`)
+          await browser.close();
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    });
+    res.status(200).json("wip");
+
+//   const browser = await puppeteer.launch({
+//     headless: true
+//   });
+//   const page = await browser.newPage();
+//   await page.setViewport({ width: 1440, height: 900 });
+//   await page.goto("https://www.instagram.com/p/Be5_t9xlHQn/", {
+//     waitUntil: "networkidle2"
+//   });
+//   const likes_count = await page.$eval(
+//     ".Nm9Fw button span",
+//     el => el.innerText
+//   );
+//   await browser.close();
+//   res.status(200).json(likes_count);
 });
 
 server.listen(7000, () =>
