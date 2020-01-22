246247a76dca7946236f9d43671c5889f8fd1d13
Maxime Salomon <maxime@croissant.io>
2020-01-22 15:56:31 +0100

    wip

--- server/server.js
+++ server/server.js
@@ -12,23 +12,23 @@ server.use(function(req, res, next) {
   next();
 });
 
+server.listen(7000, () =>
+  console.log("Server running on http://localhost:7000")
+);
+
 server.get("/", (req, res) => {
   res.send("Hello from Express");
 });
 
-const getAllLikes = (pictures) => {
-    pictures.map(pic => {
-        const userLikes = [];
-          if(pic.media_type === 'IMAGE') {
-           const likes_count = scrapeImageLikesCount(pic.permalink)
-           userLikes.push(likes_count)
-          }
-          return userLikes;
-      });
-}
+server.get("/api/max9/", async (req, res) => {
+  const allPictures = await getPictures(req.body.access_token);
+  const allLikes = await getAllLikes(allPictures);
+  res.status(200).json(allLikes);
+});
 
-const getPictures = async (access_token) => {
-    await axios
+const getPictures = access_token => {
+  return new Promise(resolve => {
+    axios
       .get("https://graph.instagram.com/me/media", {
         params: {
           fields:
@@ -36,67 +36,41 @@ const getPictures = async (access_token) => {
           access_token: access_token
         }
       })
-      .then(async res => {
-        console.log(res.data.data);
-        return getAllLikes(res.data.data)
+      .then(res => {
+        resolve(res.data.data);
       })
       .catch(err => {
         console.log(err);
       });
-} 
-
-server.get("/api/max9/", async (req, res) => {
-    console.log(req.body.user_id);
-    console.log(req.body.access_token);
-    const allPictures = await getPictures(req.body.access_token)
-    const allLikes = await getAllLikes(allPictures)
-    res.status(200).json('7');
   });
+};
 
-// server.get("/api/max9/", async (req, res) => {
-//   console.log(req.body.user_id);
-//   console.log(req.body.access_token);
-//   await axios
-//     .get("https://graph.instagram.com/me/media", {
-//       params: {
-//         fields:
-//           "id,timestamp,media_type,media_url,thumbnail_url,permalink,caption",
-//         access_token: req.body.access_token
-//       }
-//     })
-//     .then(async res => {
-//       console.log(res.data.data);
-//       await res.data.data.map(pic => {
-//         const userLikes = ['7', '12'];
-//           if(pic.media_type === 'IMAGE') {
-//            const likes_count = scrapeImageLikesCount(pic.permalink)
-//            userLikes.push(likes_count)
-//           }
-//       });
-//     })
-//     .then(() => {
-//         res.status(200).json('7');
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// });
+const getAllLikes = pictures => {
+  return new Promise(resolve => {
+    const userLikes = pictures.map(async pic => {
+      if (pic.media_type === "IMAGE") {
+        const likes_count = await scrapeImageLikesCount(pic.permalink);
+        userLikes.push(likes_count);
+      }
+    });
+    resolve(userLikes);
+  });
+};
 
-server.listen(7000, () =>
-  console.log("Server running on http://localhost:7000")
-);
-
-async function scrapeImageLikesCount(url) {
-    const browser = await puppeteer.launch({
-        headless: true
-      });
-      const page = await browser.newPage();
-      await page.setViewport({ width: 1440, height: 900 });
-      await page.goto(url, {
-        waitUntil: "networkidle2"
-      });
-      const likes_count = await page.$eval(".Nm9Fw button span", el => el.innerText);
-      await browser.close();
-      console.log(likes_count)
-      return likes_count
-}
+const scrapeImageLikesCount = async url => {
+  const browser = await puppeteer.launch({
+    headless: true
+  });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1440, height: 900 });
+  await page.goto(url, {
+    waitUntil: "networkidle2"
+  });
+  const likes_count = await page.$eval(
+    ".Nm9Fw button span",
+    el => el.innerText
+  );
+  await browser.close();
+  console.log(likes_count)
+  return likes_count
+};
