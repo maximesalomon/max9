11eab2b777bca2fd1e37bdba40d1d73ff2a377cf
Maxime Salomon <maxime@croissant.io>
2020-01-24 17:01:51 +0100

    add media_type CAROUSEL_ALBUM

--- server/server.js
+++ server/server.js
@@ -25,8 +25,9 @@ server.get("/", (req, res) => {
 server.get("/api/max9/", async (req, res) => {
   console.log(req.headers.access_token)
   const userPosts = await functions.getUserPosts(req.headers.access_token);
+  console.log(await userPosts)
   const userPictures = await userPosts.filter(
-    post => post.media_type === "IMAGE"
+    post => post.media_type === "IMAGE" || "CAROUSEL_ALBUM"
   );
   const userPicturesLikes = await functions.getUserLikes(userPictures);
   const userPicturesWithLikes = await userPictures.map( pic => {
