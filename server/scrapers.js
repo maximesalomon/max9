d8c88031fb2fbd593408fc9829992dc3a082191a
Maxime Salomon <maxime@croissant.io>
2020-01-25 11:35:54 +0100

    wip scraperLikesVideo

--- server/scrapers.js
+++ server/scrapers.js
@@ -22,12 +22,35 @@ const scrapePictureLikesCount = async url => {
 };
 
 const scrapeVideoLikesCount = async url => {
-  return;
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1440, height: 900 });
+  await page.goto(url, {
+    waitUntil: "networkidle2"
+  });
+  await setTimeout(() => {}, 5000);
+  const view_count = await page.$eval('.vcOH2 span', el => el.innerText)
+  await page.click('.vcOH2')
+  // const likes_count = await page.$eval(
+  //   ".Nm9Fw button span",
+  //   el => el.innerText
+  // );
+  await browser.close();
+  // var userLikes = {
+  //   likes_count: likes_count,
+  //   url: url
+  // };
+  // return userLikes;
+  console.log(`Video view_count = ${view_count}`)
 };
 
 const scrapeCarouseLikesCount = async url => {
   return;
 };
+
+scrapeVideoLikesCount('https://www.instagram.com/p/BZTq5d3F4KR/');
 
 module.exports = {
   scrapePictureLikesCount,
